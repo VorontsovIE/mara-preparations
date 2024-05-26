a26dbc8a2a21a1aea9cb411ca2025971ad3b97de
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2024-05-26 15:25:12 +0300

    sarus scores for besthits

--- app/convert_sarus_scoresFasta_to_tsv.rb
+++ app/convert_sarus_scoresFasta_to_tsv.rb
@@ -2,6 +2,7 @@ PATTERN = /^>(?<name>\w+)::(?<chr>chr\w+):(?<from>\d+)-(?<to>\d+)\((?<strand>[+-
 ARGF.readlines.map(&:chomp).each_slice(2){|hdr,score|
   m = hdr.match(PATTERN)
   chr, from, to, name, strand = m.named_captures.values_at('chr', 'from', 'to', 'name', 'strand')
+  score = score.split("\t").first
   info = [chr, from, to, name, score, strand]
   puts(info.join("\t"))
 }
